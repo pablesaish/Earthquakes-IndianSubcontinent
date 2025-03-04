5b38d630bdc19664b54d3949b91d7e04d015cbca
Saish Pable <pablesaish07@gmail.com>
2025-03-05 00:37:12 +0530

    adding more info

--- map.py
+++ map.py
@@ -25,7 +25,8 @@ for eq_dict in all_eq_dicts:
         eq_titles.append(eq_title)
 
 # Create the scatter map
-title = 'Earthquakes in the Indian Subcontinent'
+title = 'This project visualises earthquakes from past 5 years in the Indian Subcontinent Region'
+subtitle = 'Tech Stack: Python, Plotly | Data Source: USGS Earthquake Dataset in GeoJSON format'
 fig = px.scatter_geo(
     lat=lats,
     lon=lons,
@@ -50,12 +51,33 @@ fig.update_layout(
     title={
         'text': title,
         'font': {
-            'size': 35,  # Font size
+            'size': 30,  # Font size
             'family': 'Arial',  # Font family
             'color': 'black'  # Font color
         },
         'x': 0.5,  # Center align the title
-    }
+    },
+    annotations=[
+        {
+            'text': subtitle,
+            'xref': 'paper', 'yref': 'paper',  # Coordinate system
+            # If you use 'paper' for xref and yref, it's like saying:
+            # it puts this text anywhere on the paper"
+            # Example: A subtitle at the bottom of the map.
+
+            # If you use 'x' and 'y',
+            #"it put this text next to a specific earthquake on the map."
+            #Example: Labeling the biggest earthquake by pointing to it.
+            
+            'x': 0.5, 'y': -0.05,  # Move the subtitle belo chart x centers the text horizontally & y moves data below the char
+            'showarrow': False,  # Whether to show an arrow pointing to something
+            'font': {
+                'size': 16,  # Increase font size slightly
+                'family': 'Arial',
+                'color': 'gray'
+            }
+        }
+    ]
 )
 
 # Show the map
